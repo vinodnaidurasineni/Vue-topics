3a04e5564fe3d2a5aff64f1471929b32a2bdddb4
vinod <vinodchowdary1221@gmail.com>
2022-01-27 08:54:59 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- <img :alt="show" :src="getPic" > -->
+  <div id="app1">
+  <a :href="google">external link</a>
+  <p :class="{ boldclass : makemebold === true }">Make as bold</p>
+  <input name="email" ref="email">
+  <button @click="checkRef">Get Ref data</button>
+
+  <Modal  :name="name" :test="test" :sampleArray="[1,2,3]" @inputFocus="checkRef">
+    <p>i am in parent comp coming in child comp</p>
+    <h1>i am header</h1>
+    <template v-slot:link>
+      <a href="">LInk 1</a>
+      <a href="">link 2</a>
+    </template>
+  </Modal>
+  <!-- <Modal></Modal> -->
+  <!-- <input name="test" ref="input">
+  <p :class="{italic: show}">i am some content </p>
+  <div v-if="show">
+  <HelloWorld msg="Welcome to Your Vue.js App" @closeme="checkRef"/></div>
+  <button @click="checkRef">Focus on m e</button> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/ModalCom.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data(){
+    return{
+      makemebold: true,
+      name:"I am modal component",
+      test:" I  am another prop",
+      // show:false,
+      google:"https://google.com"
+    }
+
+  },
+  methods:{
+    checkRef(){
+      console.log(this.$refs.email);
+      this.$refs.email.focus();
+      // this.show=!this.show
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.italic{
+  background-color: black;
+}
+.boldclass{
+  color: blue;
+}
+
+p{
+  background-color: blue;
+}
+</style>
---
+++ src/components/ModalCom.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>{{ name }}</p>
+  <h1>{{ test }}</h1>
+  <p>{{ sampleArray }}</p>
+  <li v-for:="array in filterArray" @click="showMyValue(array)">
+    {{ array }}
+  </li>
+  <button @click="emitInput">Emit me for input focus</button>
+  <slot></slot>
+  <div  class="action">
+    <slot name="link"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "test", "sampleArray"],
+  methods: {
+    emitInput() {
+      this.$emit("inputFocus");
+    },
+    showMyValue(array) {
+      console.log(array);
+    },
+    filterArray() {
+      return this.sampleArray.filter((elem) => {
+        return elem > 2;
+      });
+    },
+  },
+};
+</script>
+
